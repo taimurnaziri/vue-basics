e4111f8657e717946e03e4a826abd2cac81289c5
tainazir <taimur.naziri@groupeconnect.com>
2019-05-28 12:21:56 -0400

    First commit.

---
+++ main.js
@@ -0,0 +1,11 @@
+// initiate new vue instance
+// new keyword followed by 'Vue' obj
+new Vue({
+	// el stand for element property and the 
+	// value is the class or id of that element
+	el: '#main',
+	// data obj includes all 'data' inside the defined element
+	data: {
+		title: 'Becoming a Vue Ninja'
+	}
+})
